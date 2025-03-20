f464c162022e947ba5de31910e4b17d87b5d37c2
Natali Schers <schersnatali@gmail.com>
2025-03-19 22:38:35 -0300

    [19/03] Desenvolvimento do filtro de jogos (preço, plataforma e modo)

---
+++ iaragames/js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('filterBtn').addEventListener('click', function() {
+    const price = document.getElementById('price').value;
+    const platform = document.getElementById('platform').value;
+    const mode = document.getElementById('mode').value;
+    const games = document.querySelectorAll('.jogo');
+    
+    games.forEach(game => {
+        const itemModes = game.getAttribute("data-mode").split(" ");
+        const itemPlatforms = game.getAttribute("data-platform").split(" ");
+        const itemPrices = game.getAttribute("data-price").split(" ");
+
+        const modeMatches = itemModes.includes(mode) || mode == "todos";
+        const platformMatches = itemPlatforms.includes(platform) || platform == "todas";
+        const priceMatches = itemPrices.includes(price) || price == "todos";
+
+        game.style.display = (modeMatches && platformMatches && priceMatches) ? "block" : "none";
+    });
+})
