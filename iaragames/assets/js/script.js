345e0cd3755735d24cd31b6eafc7d0fb97b803a7
Natali Schers <schersnatali@gmail.com>
2025-04-20 13:15:54 -0300

    [20/04] Criando página de jogos + ajustes nos filtros

--- iaragames/assets/js/script.js
+++ iaragames/assets/js/script.js
@@ -2,12 +2,12 @@ document.getElementById('filterBtn').addEventListener('click', function() {
     const price = document.getElementById('price').value;
     const platform = document.getElementById('platform').value;
     const mode = document.getElementById('mode').value;
-    const games = document.querySelectorAll('.jogo');
+    const games = document.querySelectorAll('.shop_card-item');
     
     games.forEach(game => {
-        const itemModes = game.getAttribute("data-mode").split(" ");
-        const itemPlatforms = game.getAttribute("data-platform").split(" ");
-        const itemPrices = game.getAttribute("data-price").split(" ");
+        const itemModes = (game.getAttribute("data-mode") || "").split(" ");
+        const itemPlatforms = (game.getAttribute("data-platform") || "").split(" ");
+        const itemPrices = (game.getAttribute("data-price") || "").split(" ");
 
         const modeMatches = itemModes.includes(mode) || mode == "todos";
         const platformMatches = itemPlatforms.includes(platform) || platform == "todas";
