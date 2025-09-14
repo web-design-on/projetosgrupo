999e4397c8696ed1c03c5ec3d465923b87d9f62d
Stephanie <webdesignfiap@gmail.com>
2025-09-14 19:08:28 -0300

    removendo código comentado do filtro de locais

--- enterprisechallenge/assets/js/filtroLocais.js
+++ enterprisechallenge/assets/js/filtroLocais.js
@@ -1,21 +1,23 @@
-document.getElementById("filtroForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+// Racunho para o filtro 
+// document.getElementById("filtroForm").addEventListener("submit", function (e) {
+//     e.preventDefault();
 
-    const esporte = document.getElementById("esporte").value;
-    const cidade = document.getElementById("cidade").value;
-    const bairro = document.getElementById("bairro").value;
-    const data = document.getElementById("data").value;
+//     const esporte = document.getElementById("esporte").value;
+//     const cidade = document.getElementById("cidade").value;
+//     const bairro = document.getElementById("bairro").value;
+//     const data = document.getElementById("data").value;
 
-    const cards = document.querySelectorAll(".card-evento");
+//     const cards = document.querySelectorAll(".card-evento");
 
-    cards.forEach((card) => {
-    let mostrar = true;
+//     cards.forEach((card) => {
+//     let mostrar = true;
 
-    if (esporte && card.dataset.esporte !== esporte) mostrar = false;
-    if (cidade && card.dataset.cidade !== cidade) mostrar = false;
-    if (bairro && card.dataset.bairro !== bairro) mostrar = false;
-    if (data && card.dataset.data !== data) mostrar = false;
+//     if (esporte && card.dataset.esporte !== esporte) mostrar = false;
+//     if (cidade && card.dataset.cidade !== cidade) mostrar = false;
+//     if (bairro && card.dataset.bairro !== bairro) mostrar = false;
+//     if (data && card.dataset.data !== data) mostrar = false;
 
-    card.style.display = mostrar ? "block" : "none";
-    });
-});
+//     card.style.display = mostrar ? "block" : "none";
+//     });
+// });
+
